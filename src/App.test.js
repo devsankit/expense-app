c00f9a51324f950bd0fe249e901968ff5c617987
devsankit <hello.ankitrathore@gmail.com>
2023-09-19 18:21:04 +0530

    Now i think Final

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('<div class="expenses-filter" data-testid="1"><div class="expenses-filter__control"><label>Filter by year</label><select><option value="2023">2023</option><option value="2022">2022</option><option value="2021">2021</option><option value="2020">2020</option></select></div></div>', () => {
   render(<App />);
   const linkTag = screen.getAllByTestId("1")
   // const linkElement = screen.getByText(<Expenses items={expenses} />/i);
