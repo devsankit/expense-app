74bbce2423145a24908a169358d2cecde863ec13
devsankit <hello.ankitrathore@gmail.com>
2023-09-19 18:13:13 +0530

    Added ID

--- src/components/Expenses/ExpensesFilter.js
+++ src/components/Expenses/ExpensesFilter.js
@@ -8,7 +8,7 @@ const ExpensesFilter = (props) => {
   };
 
   return (
-    <div className='expenses-filter'>
+    <div data-testid="1" className='expenses-filter'>
       <div className='expenses-filter__control'>
         <label>Filter by year</label>
         <select value={props.selected} onChange={dropdownChangeHandler}>
