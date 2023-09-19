5d05078284baaaeef1b99af75e08f9c543a720d4
devsankit <hello.ankitrathore@gmail.com>
2023-09-19 21:48:56 +0530

    final test

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,5 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-import renderer from "react-test-renderer"
 
 test('Expense Tracker App', () => {
   render(<App />);
